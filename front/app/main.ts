229d3ac590edb04830a9b6ce0be6b9c750fb35c9
Marek Świecznik <marek.swiecznik@rndity.com>
2017-02-18 18:32:59 +0100

    webpack introduced

--- front/app/main.ts
+++ front/app/main.ts
@@ -1,5 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
+require("../scss/main.scss");
+
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
---
+++ front/app/vendor.ts
@@ -0,0 +1,15 @@
+// Angular
+import '@angular/core';
+import '@angular/forms';
+import '@angular/http';
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/router';
+import 'angular2-cookie/core';
+import 'angular2-cookie/services/cookies.service';
+import 'moment';
+// RxJS
+import 'rxjs';
+import 'rxjs/BehaviorSubject';
+// Other vendors for example jQuery, Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
